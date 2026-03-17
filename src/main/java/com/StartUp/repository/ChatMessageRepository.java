27c2a39edc6e411c8b194faa4bab8305152a3c5f
Dilyannn <122313669+Dilyannn@users.noreply.github.com>
2026-03-17 03:07:36 +0200

    Update ChatMessageRepository.java

--- src/main/java/com/StartUp/repository/ChatMessageRepository.java
+++ src/main/java/com/StartUp/repository/ChatMessageRepository.java
@@ -2,10 +2,12 @@ package com.StartUp.repository;
 
 import com.StartUp.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
     List<ChatMessage> findByUserIdOrderByCreatedAtAsc(Long userId);
+    @Transactional
     void deleteByUserId(Long userId);
 }
